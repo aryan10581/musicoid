323c51f4e8212092929043132df198ef119d73d1
aryan10581 <aryanrajput1058@gmail.com>
2022-11-22 19:21:53 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/globals.css'
+import "./font.css"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider >
+    <Component {...pageProps} />
+  </ChakraProvider>
+    )
+    
 }
 
 export default MyApp
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,649 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({
+    "songs":
+      [
+        {
+          "id": 1,
+          "song": "Illahi",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/Illahi.mp3",
+          "duration": "3:23",
+          "author": "Roy"
+        },
+        {
+          "id": 2,
+          "song": "Sooraj Dooba Hai",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/SoorajDoobaHain.mp3",
+          "duration": "4:07",
+          "author": "Arijit Singh"
+
+        },
+        {
+          "id": 3,
+          "song": "Baaki Baatien Peene Baad",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/Baakibaate.mp3",
+          "duration": "3:40",
+          "author": "Arjun"
+
+        },
+        {
+          "id": 4,
+          "song": "Kar Har Maidaan Fateh",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/Karharmaidan.mp3",
+          "duration": "3:31",
+          "author": "Rajkumar Hirani"
+
+        },
+        {
+          "id": 5,
+          "song": "Vardaan",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/Vardan.mp3",
+          "duration": "3:01",
+          "author": "Ajey Nagar (Carryminati)"
+
+        },
+        {
+          "id": 6,
+          "song": "Zinda",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/Zinda.mp3",
+          "duration": "3:29",
+          "author": "Farhan Akhtar"
+
+        },
+        {
+          "id": 7,
+          "song": "Yalgaar",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/Yalgaar.mp3",
+          "duration": "3:14",
+          "author": "Ajey Nagar (Carryminati)"
+
+        },
+        {
+          "id": 8,
+          "song": "Zindagi",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/Zindagi.mp3",
+          "duration": "3:47",
+          "author": "Amit Trivedi"
+
+        },
+        {
+          "id": 9,
+          "song": "Dil Dhadakne Do",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/Dildhadak.mp3",
+          "duration": "3:23",
+          "author": "Joi Barua"
+
+        },
+        {
+          "id": 10,
+          "song": "Jee Karda",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/Jeekarda.mp3",
+          "duration": "3:59",
+          "author": "Divya Kumar"
+
+        },
+        {
+          "id": 11,
+          "song": "Sadda Haq",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/Saddahaq.mp3",
+          "duration": "6:08",
+          "author": "Fakhri"
+
+        },
+        {
+          "id": 12,
+          "song": "Bezubaan",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/Bezubaan.mp3",
+          "duration": "2:59",
+          "author": "Tanvi Shah"
+
+        },
+        {
+          "id": 13,
+          "song": "Amplifier",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/Amplifier.mp3",
+          "duration": "3:12",
+          "author": "Imran Khan"
+
+        },
+        {
+          "id": 14,
+          "song": "Lee Ja Tu Mujhe",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/Lejaa.mp3",
+          "duration": "5:33",
+          "author": "Atif Aslam"
+
+        },
+        {
+          "id": 15,
+          "song": "Nayi Subah",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/NayiSubah.mp3",
+          "duration": "3:40",
+          "author": "Jigar Sariya"
+
+        },
+        {
+          "id": 16,
+          "song": "Unstoppable",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/Unstopable.mp3",
+          "duration": "3:35",
+          "author": "Sia"
+
+        },
+        {
+          "id": 17,
+          "song": "No Roots",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/Noroots.mp3",
+          "duration": "3:56",
+          "author": "Alice Martin"
+
+        },
+        {
+          "id": 18,
+          "song": "Something Just Like This",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/Somethingjust.mp3",
+          "duration": "4:07",
+          "author": "Chainsmokers"
+
+        },
+        {
+          "id": 19,
+          "song": "Blinding Lights",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/BlindingLights.mp3",
+          "duration": "3:20",
+          "author": "Weeknd"
+
+        },
+        {
+          "id": 20,
+          "song": "All We Know",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/Allweknow.mp3",
+          "duration": "3:16",
+          "author": "Chainsmokers"
+
+        },
+        {
+          "id": 21,
+          "song": "Dusk Till Dawn Luca Lush Remix",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/dusktill.mp3",
+          "duration": "3:27",
+          "author": "Luca Lush"
+
+        },
+        {
+          "id": 22,
+          "song": "Save Your Tears",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/Saveyour.mp3",
+          "duration": "3:36",
+          "author": "Weeknd"
+
+        },
+        {
+          "id": 23,
+          "song": "Bhaag D.K. Bose",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/bhagdk.mp3",
+          "duration": "3:08",
+          "author": "Delhi Belly"
+
+        },
+        {
+          "id": 24,
+          "song": "Sunn Raha Hai Na Tu",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/sunrahahai.mp3",
+          "duration": "06:30",
+          "author": "Aditya Roy Kapur"
+
+        },
+        {
+          "id": 25,
+          "song": "Tum Hi Ho",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/tumhiho.mp3",
+          "duration": "04:27",
+          "author": "Aditya Roy Kapur"
+
+        },
+        {
+          "id": 26,
+          "song": "MERI AASHIQUI",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/meriaashqui.mp3",
+          "duration": "04:18",
+          "author": "Arijit Singh"
+
+        },
+        {
+          "id": 27,
+          "song": "Milne Hai Mujhse Aayi",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/Milnehaimujhse.mp3",
+          "duration": "04:55",
+          "author": "Aditya Roy Kapur"
+
+        },
+        {
+          "id": 28,
+          "song": "Aashayein",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/aashaiye.mp3",
+          "duration": "04:19",
+          "author": "(K.K)"
+
+        },
+        {
+          "id": 29,
+          "song": "Moth To A Flame ",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/mothtoaflame.mp3",
+          "duration": "03:55",
+          "author": " The Weeknd "
+
+        },
+        {
+          "id": 30,
+          "song": "Titanium",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/titanium.mp3",
+          "duration": "03:56 ",
+          "author": "David Guetta"
+
+        },
+        {
+          "id": 31,
+          "song": "All For Love",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/allforlove.mp3",
+          "duration": "03:01",
+          "author": "Tungevaag & Raaban"
+
+        },
+        {
+          "id": 32,
+          "song": "BESABRIYAAN",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/besabriyaan.mp3",
+          "duration": "04:30",
+          "author": " M. S. DHONI - THE UNTOLD STORY"
+
+        },
+        {
+          "id": 33,
+          "song": "Udd Gaye",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/Uddgaye.mp3",
+          "duration": "03:02",
+          "author": "Ritviz "
+
+        },
+        {
+          "id": 34,
+          "song": "Faded Lost Stories Remix",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/faded.mp3",
+          "duration": "03:31",
+          "author": "Alan Walker "
+
+        },
+        {
+          "id": 35,
+          "song": "Sahara",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/sahara.mp3",
+          "duration": "02:51",
+          "author": "Hensonn"
+
+        },
+        {
+          "id": 36,
+          "song": "Stereo Hearts",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/stereohearts.mp3",
+          "duration": "03:35",
+          "author": "Gym Class Heroes"
+
+        },
+        {
+          "id": 37,
+          "song": "Alone, Pt. II",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/alonept2.mp3",
+          "duration": "02:57",
+          "author": "Alan Walker"
+
+        },
+        {
+          "id": 38,
+          "song": "Excuses",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/Excuses.mp3",
+          "duration": "02:57",
+          "author": "AP Dhillon"
+
+        },
+        {
+          "id": 39,
+          "song": "Invisible ",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/invisible.mp3",
+          "duration": " 03:07",
+          "author": "Anna Clendening"
+
+        },
+        {
+          "id": 40,
+          "song": "Ignite",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/ignite.mp3",
+          "duration": "03:27",
+          "author": "Alan Walker"
+
+        },
+        {
+          "id": 41,
+          "song": "The Nights",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/thenights.mp3",
+          "duration": "02:53",
+          "author": "Avicii"
+
+        },
+        {
+          "id": 42,
+          "song": "No Money",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/nomoney.mp3",
+          "duration": "02:53",
+          "author": "Vapi"
+
+        },
+        {
+          "id": 43,
+          "song": "Alone",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/alone.mp3",
+          "duration": "02:41",
+          "author": "Alan Walker"
+
+        },
+        {
+          "id": 44,
+          "song": "Youngblood ",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/youngblood.mp3",
+          "duration": "03:06",
+          "author": "Arc north "
+
+        },
+        {
+          "id": 45,
+          "song": "Superhero",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/superhero.mp3",
+          "duration": "03:02",
+          "author": "Unknown Brain"
+
+        },
+        {
+          "id": 46,
+          "song": "Brown Munde",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/brownmunde.mp3",
+          "duration": "04:01",
+          "author": "AP Dhillon"
+
+        },
+        {
+          "id": 47,
+          "song": "Satisfya",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/satisfaya.mp3",
+          "duration": " 03:04 ",
+          "author": "AP Dhillon"
+
+        },
+     {
+          "id": 48,
+          "song": "Shape of You",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/shapeofyou.mp3",
+          "duration": "03:54",
+          "author": "Ed Sheeran"
+
+        },
+  {
+          "id": 49,
+          "song": "Wahran ",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/wahran.mp3",
+          "duration": "04:03",
+          "author": "RANDALL "
+
+        },
+  {
+          "id": 50,
+          "song": "Desi Kalakaar",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/desikalakaar.mp3",
+          "duration": "04:03",
+          "author": "Honey Singh"
+
+        },
+ {
+          "id": 51,
+          "song": "Enemy ",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/enemy.mp3",
+          "duration": "02:50",
+          "author": "Imagine Dragons"
+
+        },
+ {
+          "id": 52,
+          "song": "Wavin Flag ",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/wavinflad.mp3",
+          "duration": "03:45",
+          "author": "K'Naan"
+
+        },
+ {
+          "id": 53,
+          "song": "Dope Shope",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/dopeshope.mp3",
+          "duration": "03:08",
+          "author": "Yo Yo Honey Singh"
+
+        },
+{
+          "id": 54,
+          "song": "Legends Never Die",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/legendsneverdie.mp3",
+          "duration": "02:47",
+          "author": "Alan Walker"
+
+        },
+{
+          "id": 55,
+          "song": "Magic In The Air",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/magicintheair.mp3",
+          "duration": "03:53",
+          "author": "Magic System"
+
+        },
+{
+          "id": 56,
+          "song": "Cheap Thrills",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/cheapthrils.mp3",
+          "duration": "03:39",
+          "author": "Sia"
+
+        },
+{
+          "id": 57,
+          "song": "Closer ",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/closer.mp3",
+          "duration": "04:05",
+          "author": "The Chainsmokers"
+
+        },
+{
+          "id": 58,
+          "song": "Silence",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/silence.mp3",
+          "duration": "03:01",
+          "author": "Marshmello"
+
+        },
+{
+          "id": 59,
+          "song": "On & On",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/onandon.mp3",
+          "duration": "03:27",
+          "author": "Cartoon "
+
+        },
+{
+          "id": 60,
+          "song": "Bad Liar",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/badliar.mp3",
+          "duration": "04:18  ",
+          "author": "Imagine Dragons "
+
+        },
+{
+          "id": 61,
+          "song": " Astronaut In The Ocean",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/astroinocean.mp3",
+          "duration": "02:15",
+          "author": "Masked Wolf"
+
+        },
+{
+          "id": 62,
+          "song": "Treat You Better",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/treatubetter.mp3",
+          "duration": " 03:05",
+          "author": "Shawn Mendes"
+
+        },
+{
+          "id": 63,
+          "song": "Scared To Be Lonely",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/scaredtobelonely.mp3",
+          "duration": "03:41",
+          "author": "Martin Garrix & Dua Lipa"
+
+        },
+{
+          "id": 64,
+          "song": "Cradles",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/cradles.mp3",
+          "duration": "03:30",
+          "author": "Sub Urban"
+
+        },
+{
+          "id": 65,
+          "song": "Lily ",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/lily.mp3",
+          "duration": "03:16",
+          "author": "Alan Walker"
+
+        },
+{
+          "id": 66,
+          "song": "Demons",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/demons.mp3",
+          "duration": "02:54",
+          "author": "Imagine Dragons"
+
+        },
+{
+          "id": 67,
+          "song": "Faded",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/fadedtiesto.mp3",
+          "duration": "04:29",
+          "author": "Alan Walker"
+
+        },
+{
+          "id": 68,
+          "song": "Heroes Tonight",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/heroestonight.mp3",
+          "duration": "03:29",
+          "author": "Janji"
+
+        },
+{
+          "id": 69,
+          "song": "The Spectre",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/spectre.mp3",
+          "duration": "03:25",
+          "author": "Alan Walker"
+
+        },
+{
+          "id": 70,
+          "song": "What Makes You Beautiful",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/whatmakesyoubeautiful.mp3",
+          "duration": "03:35",
+          "author": "One Direction"
+
+        },
+{
+          "id": 71,
+          "song": "One Thing",
+          "img": "https://source.unsplash.com/featured/",
+          "audio": "/Songs/MP3/onething.mp3",
+          "duration": "03:16",
+          "author": "One Direction"
+
+        },
+
+      ]
+
+  })
 }
